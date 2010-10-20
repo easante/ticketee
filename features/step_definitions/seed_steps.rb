f1e82e69c43016c82ef42548b00c7098ed2e0e5c
Emmanuel Asante <emmanuela.asante@gmail.com>
2010-10-19 18:27:59 -0700

    Implemented deletion functionality for projects

--- features/step_definitions/seed_steps.rb
+++ features/step_definitions/seed_steps.rb
@@ -1,3 +1,7 @@
 Given /^there is a project called "([^\"]*)"$/ do |name|
   Project.create!(:name => name)
 end
+
+#Given /^there is a project called "([^\"]*)"$/ do |name|
+#  Project.delete!(name)
+#end
